e06730a27c3fe72638b53b1bac512290a72643a3
Paweł Suchta <pawel.suchta@polcode.net>
2020-03-06 12:49:59 +0100

    Delete Unnecessary Comment

--- app/jobs/update_query_results.rb
+++ app/jobs/update_query_results.rb
@@ -1,21 +1,3 @@
-# class UpdateQueryResults < ApplicationJob
-#   queue_as :default
-
-#   # after_perform do |job|
-#   #   self.class.set(wait: 10.seconds).perform_later(job.arguments.first)
-#   # end
-
-#   def preform(query_id)
-#     Rails.logger.debug "[Before find] [#{self.class}] [#{job_id}] #{e}"
-#     query = Query.find_by(query_id)
-#     Rails.logger.debug "[After find] [#{self.class}] [#{job_id}] #{e}"
-#     return unless query.present?
-#     query.update_or_create_query_results
-#     Rails.logger.debug "[Return] [#{self.class}] [#{job_id}] #{e}"
-#   end
-# end
-
-
 class UpdateQueryResults < Struct.new(:query_id)
 
   def perform
