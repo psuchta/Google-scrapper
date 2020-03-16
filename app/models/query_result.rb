e8200a99002366eb44af49c8bba4dc934e2bd16f
Pawel Suchta <pawel.suchta@polcode.net>
2020-03-16 08:41:10 +0100

    Fixes after merge comments - query_result, gemfile

--- app/models/query_result.rb
+++ app/models/query_result.rb
@@ -2,8 +2,8 @@ class QueryResult < ApplicationRecord
   belongs_to :query, required: true
 
   def self.create_or_update_by_link!(query_id:, text:, link:)
-    raise ActiveRecord::RecordNotFound unless ::Query.exists?(query_id)
-    query_result = find_by_query_id_and_link(query_id, link)
+    query = ::Query.find(query_id)
+    query_result = find_by_query_id_and_link(query, link)
     if query_result.present?
       query_result.update!(text: text)
       query_result
