f4f967d41ff799a90aefb2b76b998e3b8ecbdda3
Pawel Suchta <pawel.suchta@polcode.net>
2020-03-18 12:04:47 +0100

    change search_queries_controller featue specs

--- spec/features/search_queries_controller/delete_query_spec.rb
+++ spec/features/search_queries_controller/delete_query_spec.rb
@@ -1,8 +1,9 @@
 describe 'delete query' do
   it 'redirects succesfully', :vcr do
-    query = create(:search_query)
+    create(:search_query)
     visit search_queries_path
     click_link 'Delete'
     expect(page).to have_content('Query destroyed!')
+    expect(page.current_path).to eq(search_queries_path)
   end
 end
--- spec/features/search_queries_controller/send_search_query_spec.rb
+++ spec/features/search_queries_controller/send_search_query_spec.rb
@@ -1,12 +1,13 @@
 describe 'Send search query' do
   context 'with string parameter' do
-    it 'redirects succesfully', :vcr do
+    it 'redirects succesfully and displays search_query object', :vcr do
       visit search_queries_path
       within('#new_search_query') do
         fill_in 'search_query_searched_quote', with: 'Rails'
       end
       click_button 'Search'
       expect(page).to have_content('Query created!')
+      expect(page).to have_content('Rails')
     end
   end
 
