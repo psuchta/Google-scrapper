48d4f413b71f1f7b180b05b14b7a448558c0f510
Pawel Suchta <pawel.suchta@polcode.net>
2020-03-19 16:12:56 +0100

    add #update #edit #show action tests in requests/search_queriies_management.spec

--- spec/requests/search_queries_management_spec.rb
+++ spec/requests/search_queries_management_spec.rb
@@ -7,6 +7,22 @@ describe 'SearchQueries Management' do
     end
   end
 
+  describe 'GET #show' do
+    it 'gets successful response' do
+      search_query = create(:search_query)
+      get search_query_path(search_query)
+      expect(response).to be_successful
+    end
+  end
+
+  describe 'GET #edit' do
+    it 'gets successful response' do
+      search_query = create(:search_query)
+      get edit_search_query_path(search_query)
+      expect(response).to be_successful
+    end
+  end
+
   describe 'POST #create' do
     let(:create_action) { post search_queries_path, params: params }
 
@@ -26,12 +42,27 @@ describe 'SearchQueries Management' do
   end
 
   describe 'DELETE #destroy' do
-    let(:delete_action) { delete search_query_path(query), params: { id: query } }
     let!(:query) { create(:search_query_with_query_results, query_results_count: 3) }
+    let(:delete_action) { delete search_query_path(query)}
 
     it 'deletes the objects and redirects to #index' do
       expect { delete_action }.to change(SearchQuery, :count).by(-1)
         .and change(QueryResult, :count).by(-3)
+      expect(response).to redirect_to(search_queries_path)
+    end
+  end
+
+  describe 'PUT #update', :vcr do
+    let!(:query) { create(:search_query_with_query_results, query_results_count: 3) }
+    let(:params) do
+      {
+        search_query: { searched_quote: 'Rails' }
+      }
+    end
+    let(:update_action) { put search_query_path(query), params: params }
+
+    it 'updates search query and redirects to #index' do
+      expect { update_action }.to change(QueryResult, :count).by(12)
       expect(response).to redirect_to(search_queries_path)
     end
   end
